ba44c225e7bdacf54aa449a3c4786494b83e62b9
nnd1402 <nenadmihajlovic1402@gmail.com>
2021-03-22 13:43:22 +0100

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Particles, { ShapeType } from 'react-particles-js'
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
-   <Navbar />
+  <>
+    <Particles
+      className="particles-canvas" 
+      params={{
+        particles: {
+          number: {
+            value: 30,
+            density: {
+              enable: true,
+              value_area: 900
+            }
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 6,
+              color: "#f9ab00"
+            }
+          }
+        }
+      }}
+
+    />
+    <Navbar />
+    <Header />
+  </>
   )
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Typed from 'react-typed'
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>Web development and UI/UX</h1>
+                <Typed className="typed-text"
+                strings={["Web Design", "Web Development"]}
+                typeSpeed={40}
+                backSpeed={60}
+                loop
+                />
+                <a className="btn-main-offer" href="">Let's Talk</a> 
+            </div>
+        </div>
+    )
+}
+
+export default Header
